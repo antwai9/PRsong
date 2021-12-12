5025ecd97b3f4f67bcc0cdc2b79a611f2e9b5fc4
hadohu <89905375+hadohu@users.noreply.github.com>
2021-12-12 12:05:53 +0200

    Delete __init__.py

--- asadmodules/__init__.py
+++
@@ -1,9 +0,0 @@
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-# Without Credit (Mother Fucker)
-# Rocks © @x_U_S_A © Rocks
-# Owner Neyork
-
-__version__ = "0.6.0"
-
-# Unknown
-# Roses are red, Violets are blue, A face like yours, Belongs in a zoo.
